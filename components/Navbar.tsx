cdb813a03c192b7789b217c4ded98e05a06616b5
Jason Warner <jbw080202@gmail.com>
2025-03-05 10:28:13 -0600

    Update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,33 +3,44 @@ import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import Searchbar from "@/components/Searchbar";
+
 export default function Navbar() {
   return (
     <Card className="flex flex-row justify-between px-5 py-3 rounded-none">
       <Link href="/" className="flex flex-row gap-2">
-        <Image src="/Logo.png" alt="Logo" height={50} width={50} />
-        <h1 className="content-center">AdventShare</h1>
+        <Image src="/Logo2.png" alt="Logo" height={75} width={75} />
+        <h1 className="content-center w-48">AdventShare</h1>
       </Link>
-      <Searchbar />
-      <div className="flex flex-row gap-5 items-center">
-        <div className="hidden md:flex flex-row gap-5 ">
+      <SignedIn>
+        <div className="flex-1 flex items-center justify-center max-w-xl mx-auto">
+          <Searchbar />
+        </div>
+      </SignedIn>
+      <div className="flex flex-row gap-5 items-center text-lg">
+        <div
+          className="hidden md:flex flex-row gap-5"
+          style={{ color: "#003B5C" }}
+        >
           <Link href="/post-item" className="p-2">
             Post
           </Link>
-          <Link href="/items" className="p-2">
+          <Link href="/claims" className="p-2">
             Claims
           </Link>
-          <Link href="/profile" className="p-2">
+          <Link href="/Profile" className="p-2">
             Profile
           </Link>
-          <div className="cursor-pointer content-center">
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
+        </div>
+        <div
+          className="cursor-pointer content-center text-lg"
+          style={{ color: "#003B5C" }}
+        >
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </div>
     </Card>
