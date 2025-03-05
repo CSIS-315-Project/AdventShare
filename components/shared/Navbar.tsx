ea6a3b14ec316e886b8e4cc499470d77bdadc2ce
Jason Warner <jbw080202@gmail.com>
2025-03-05 10:30:52 -0600

    Back to shared for complex merge conflict.

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,7 +2,7 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
-import Searchbar from "@/components/Searchbar";
+import Searchbar from "@/components/shared/Searchbar";
 
 export default function Navbar() {
   return (
