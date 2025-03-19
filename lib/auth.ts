4dc8ed4aa1956cd7fc5da1a1df916f78a2d0dc62
Jason Warner <jbw080202@gmail.com>
2025-03-19 10:50:52 -0500

    Admin Organizations. BREAKING CHANGES

---
+++ lib/auth.ts
@@ -0,0 +1,7 @@
+import "server-only";
+
+import { createClerkClient } from '@clerk/backend'
+
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
+
+export default clerkClient;
