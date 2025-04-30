f2a66bc954f79f32b9cd8e1615a98b8dc21e5f70
Jason Warner <jbw080202@gmail.com>
2025-04-29 22:41:58 -0500

    Update supabase client to be in supabase folder. Update layout for /posts directory.

---
+++ app/(portal)/(dashboard)/posts/(default)/layout.tsx
@@ -0,0 +1,18 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { PostSidebar } from "@/features/posts/components/sidebar/Sidebar";
+import { Separator } from "@/components/ui/separator";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <PostSidebar postId={null} />
+      <div className="w-full px-3">
+        <div className="py-4 flex flex-row">
+          <SidebarTrigger />
+        </div>
+        <Separator />
+        {children}
+      </div>
+    </>
+  );
+}
--- app/(portal)/(dashboard)/posts/layout.tsx
+++ app/(portal)/(dashboard)/posts/layout.tsx
@@ -5,14 +5,7 @@ import { Separator } from "@/components/ui/separator";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider className="w-full">
-      <PostSidebar postId={null} />
-      <div className="w-full px-3">
-        <div className="py-4 flex flex-row">
-          <SidebarTrigger />
-        </div>
-        <Separator />
       {children}
-      </div>
     </SidebarProvider>
   );
 }
